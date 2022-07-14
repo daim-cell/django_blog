eed059553c5f8b4a8d5d5a88e996e051733a68da
Daim <daimabbask01@yahoo.com>
2022-07-15 04:37:15 +0500

    Added bunch of html

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-
+posts =[
+    {
+        'author':'Daim',
+        'title':'First Blog',
+        'content': 'First blog for the site',
+        'date_posted': '15/07/2022'
+    }
+]
 def home(request):
-    return HttpResponse('good')
+    cont = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', cont)
