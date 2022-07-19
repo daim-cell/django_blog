69d8688cde21e0a3cd55f8ccfbe6cf45f408289d
Daim <daimabbask01@yahoo.com>
2022-07-19 05:06:27 +0500

    Added picture with each blog

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from distutils.command.upload import upload
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,3 +11,11 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return f'{self.user.username}'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if (img.height >300 or img.width >300):
+             img.thumbnail((600,600))
+             img.save(self.image.path)
+
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from email import message
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
@@ -23,4 +23,23 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Account Updated')
+            return redirect('profile')
+        else:
+            messages.warning(request, 'Enter correct information')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    return render(request, 'users/profile.html',
+        {
+            'u_form' : u_form,
+            'p_form' : p_form
+        }
+    )
