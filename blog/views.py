1d3c4fe74ff61d5d9dfc3efaf1b0ea1f506d966b
Daim <daimabbask01@yahoo.com>
2022-07-21 23:03:23 +0500

    Filter by user to view his posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 # Create your views here.
 
 posts =[
@@ -23,12 +24,23 @@ def home(request):
     }
     return render(request, 'blog/home.html', cont)
 
-# @login_required
 class PostListView(LoginRequiredMixin , ListView):
     model = Post
     context_object_name = 'posts'
     template_name= 'blog/home.html'
     ordering = ['-date_posted']
+    paginate_by: int = 5
+
+class UserPostListView(LoginRequiredMixin , ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name= 'blog/user_posts.html'
+    paginate_by: int = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author = user).order_by('-date_posted')
+
 
 class PostDetailView(LoginRequiredMixin,DetailView):
     model = Post
